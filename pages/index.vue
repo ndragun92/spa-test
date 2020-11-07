0cec09d804d5fed0896fd219cc5d83ed921fedd2
ndragun <39500267+ndragun92@users.noreply.github.com>
2020-11-07 02:18:19 +0100

    test asyncData

--- pages/index.vue
+++ pages/index.vue
@@ -1,36 +1,27 @@
 <template>
   <div>
-    <ul v-if="!$fetchState.pending">
+    <ul>
       <li v-for="post in posts" :key="post.id">
         <nuxt-link :to="`/post/${post.id}`">
           {{ post.title }}
         </nuxt-link>
       </li>
     </ul>
-    <div v-else>
-      Loading...
-    </div>
   </div>
 </template>
 
 <script>
 export default {
-  async fetch () {
-    await this.getPosts()
-  },
-  data: () => ({
-    posts: []
-  }),
-  methods: {
-    async getPosts () {
-      try {
-        const response = await this.$axios.$get('https://jsonplaceholder.typicode.com/posts')
-        // console.log('response get all posts', response)
-        this.posts = response
-      } catch (e) {
-        console.log('error getting posts')
-      }
+  async asyncData ({ $axios }) {
+    let posts = []
+    try {
+      const response = await $axios.$get('https://jsonplaceholder.typicode.com/posts')
+      // console.log('response get all posts', response)
+      posts = response
+    } catch (e) {
+      console.log('error getting posts')
     }
+    return { posts }
   }
 }
 </script>
