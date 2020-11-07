7d0a4c5f7e1806de1b08f23599d80916c8b44e98
ndragun <39500267+ndragun92@users.noreply.github.com>
2020-11-07 02:15:17 +0100

    test

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,6 +10,9 @@ export default {
   ** See https://nuxtjs.org/api/configuration-target
   */
   target: 'static',
+  generate: {
+    fallback: true
+  },
   /*
   ** Headers of the page
   ** See https://nuxtjs.org/api/configuration-head
